cc8b8312e83f1bbcf479d250836a723f561a0652
mauroradino <mauroradino2003@gmail.com>
2023-05-29 20:23:18 -0300

    starting cart

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+let seleccionados = [];
+
+
+class producto{
+  constructor (nombre, descripcion, precio, cantidad){
+      this.nombre = nombre,
+      this.descripcion = descripcion,
+      this.precio = precio,
+      this.cantidad = cantidad
+  }
+}
+
+const WH1000XM4B = new producto(WH1000XM4B,"","$50",0)
+const TUNE500BT = new producto(TUNE500BT,"","$50",0)
+const Live660NC= new producto(Live660NC,"","$50",0)
+const JBL650BTNC= new producto(JBL650BTNC,"","$50",0)
+const Verizon = new producto(Verizon,"","$50",0)
+const G335 = new producto(G335,"","$50",0)
+const G733 = new producto(G733,"","$50",0)
+const ZONEVIBE100 = new producto(ZONEVIBE100,"","$50",0)
+const AIRPODSPROMAX = new producto(AIRPODSPROMAX,"","$50",0)
+
+
+
+
+
+
+
+
+/*________________NAVBAR___________________*/ 
 let arrow = document.querySelectorAll(".arrow");
 for (var i = 0; i < arrow.length; i++) {
   arrow[i].addEventListener("click", (e)=>{
@@ -12,3 +42,4 @@ console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
 });
+/*________________NAVBAR___________________*/ 
